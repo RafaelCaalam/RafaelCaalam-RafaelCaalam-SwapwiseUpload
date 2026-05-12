7401cd604a415dc9d98f9f42c2f784251ca27e7d
RafaelCaalam <164004380+RafaelCaalam@users.noreply.github.com>
2026-05-13 00:00:34 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,35 +132,14 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
     "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "http://localhost:5176",
-    "http://localhost:5177",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
-    "http://127.0.0.1:5175",
-    "http://127.0.0.1:5176",
-    "http://127.0.0.1:5177",
-    "http://127.0.0.1:3000",
+    "https://rafael-caalam-rafael-caalam-swapwis.vercel.app", # I-add ito nang saktong ganito
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "http://localhost:5176",
-    "http://localhost:5177",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
-    "http://127.0.0.1:5175",
-    "http://127.0.0.1:5176",
-    "http://127.0.0.1:5177",
-    "http://127.0.0.1:3000",
+    "https://rafael-caalam-rafael-caalam-swapwis.vercel.app",
 ]
 
 MEDIA_URL = '/media/'
